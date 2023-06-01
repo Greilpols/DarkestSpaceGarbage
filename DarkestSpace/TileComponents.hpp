d814727d274559621849e0636bc80a456cffb6e5
Daniel Skogli <daniel.skogli89@gmail.com>
2023-06-01 23:51:15 +0300

    starting to create TileComponent

---
+++ DarkestSpace/TileComponents.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ECS.hpp"
+#include "PositionComponent.hpp"
+#include <SDL.h>
+
+class TileComponent : Component
+{
+	PositionComponent* transform;
+	SpriteComponent* sprite;
+
+	SDL_Rect tileRect;
+	int tileID;
+	char* path;
+
+	TileComponent() = default;
+
+	TileComponent(int x, int y, int w, int h, int id)
+	{
+		tileRect.x = x;
+		tileRect.y = y;
+		tileRect.w = w;
+		tileRect.h = h;
+		tileID = id;
+	}
+};
