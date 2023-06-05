bc86266cafed759bc9c6bd7a10171f196f78d84f
Daniel Skogli <daniel.skogli89@gmail.com>
2023-06-05 23:59:49 +0300

    tidying up map, preparing for including map tileset directly

--- DarkestSpace/Map.hpp
+++ DarkestSpace/Map.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "Game.hpp"
 
 class Map {
 
@@ -8,19 +7,8 @@ public:
 	Map();
 	~Map();
 
-	void LoadMap(int arr[20][25]);
-	void DrawMap();
-
-
+	void LoadMap(std::string path, int sizeX, int sizeY);
 
 private:
 
-	SDL_Rect src, dest;
-
-	SDL_Texture* dirt;
-	SDL_Texture* grass;
-	SDL_Texture* water;
-
-	int map[20][25];
-
-}
+};
