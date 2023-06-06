8711efc7f1adaead5521c7f08df0a17a41affc67
Daniel Skogli <daniel.skogli89@gmail.com>
2023-06-06 23:11:56 +0300

    cycle through the map tiles and adding them in game

--- DarkestSpace/Map.cpp
+++ DarkestSpace/Map.cpp
@@ -17,7 +17,15 @@ void Map::LoadMap(std::string path, int sizeX, int sizeY) {
 	std::fstream mapFile;
 	mapFile.open(path);
 
-
+	for (int y = 0; y < sizeY; y++)
+	{
+		for (int x = 0; x < sizeX; x++)
+		{
+			mapFile.get(tile);
+			Game::AddTile(atoi(&tile), x * 32, y * 32);
+			mapFile.ignore();
+		}
+	}
 
 	mapFile.close();
 }
