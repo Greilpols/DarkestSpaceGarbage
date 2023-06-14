35601ba8cb77081e3aa32e4dfee89ca1e58f93ec
Daniel Skogli <daniel.skogli89@gmail.com>
2023-06-14 23:48:42 +0300

    AssetManagement needed, so started it

---
+++ DarkestSpace/AssetManager.hpp
@@ -0,0 +1,17 @@
+#pragma once;
+
+#include <map>
+#include <string>
+#include "TextureManager.hpp"
+#include "Vector2D.hpp"
+#include "ECS.hpp"
+
+class AssetManager
+{
+public:
+	AssetManager(Manager* man);
+	~AssetManager();
+
+private:
+
+};
