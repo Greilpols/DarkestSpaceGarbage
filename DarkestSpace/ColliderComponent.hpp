6662cef4b9c60c931857c1bbd0e980566c5b1fd1
Daniel Skogli <daniel.skogli89@gmail.com>
2023-07-11 23:25:04 +0300

    bug hunting - wrong file asset called

--- DarkestSpace/ColliderComponent.hpp
+++ DarkestSpace/ColliderComponent.hpp
@@ -37,7 +37,7 @@ public:
 		}
 		transform = &entity->getComponent<PositionComponent>();
 
-		tex = TextureManager::LoadTexture("collish/test.png");
+		tex = TextureManager::LoadTexture("assets/test.png");
 		srcR = { 0, 0, 32, 32 };
 		destR = { collider.x, collider.y, collider.w, collider.h };
 
